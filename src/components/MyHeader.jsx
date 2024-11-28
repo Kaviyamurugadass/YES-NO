c5fc09c131c9acd05a7357a78b3d3aa0c9b56236
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-11-28 18:10:10 +0530

    commit

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,6 +1,6 @@
 export default function MyHeader(){
     return(
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 text-center">
+        <div className="absolute top-12 md:top-0 left-1/2 transform -translate-x-1/2 text-center">
             <h1 className="text-4xl md:text-6xl font-semibold py-3 md:p-4 drop-shadow-[2px_4px_2px_rgba(0,0,0,1)]">
                 <span className="text-[#00ff00]">YES</span>/<span className="text-[rgb(255,64,0)]">NO</span>
             </h1>
